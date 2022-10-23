5f7eb585d16442f325077d65278f37eba56c277e
Marwin <marwinbodgan@gmail.com>
2022-10-23 14:57:54 -0400

    Creando validacion del Email

--- validacion.js
+++ validacion.js
@@ -1,19 +1,19 @@
 // Haz tú validación en javascript acá
 
-const elementosimputs = document.querySelectorAll('input');
-elementosimputs.forEach(elemento => {
-    elemento.addEventListener('blur', elemento=>{
-        console.log(elemento.target);
-        console.log(elemento.target.validity.customError);
-    })
-});
+// const elementosimputs = document.querySelectorAll('input');
+// elementosimputs.forEach(elemento => {
+//     elemento.addEventListener('blur', elemento=>{
+//         console.log(elemento.target);
+//         console.log(elemento.target.value);
+//         console.log(elemento.target.validity.valid);
+//     })
+// });
 
 const elementonombre = document.querySelector('#nombre');
 elementonombre.addEventListener('blur', elementonombre =>{
     validarNombre(elementonombre.target)
 })
 function validarNombre(elemento){
-    console.log(elemento.validity.valid + " "+ elemento.classList);
     if (elemento.validity.valid){
         elemento.classList.remove('formcontato__input-error');
         // elemento.classList.add('formcontato__input');
@@ -22,4 +22,14 @@ function validarNombre(elemento){
         elemento.classList.add('formcontato__input-error');
         elemento.setCustomValidity('El campo Nombre no puede estar vacio');
     }
+}
+
+//
+const elementoemail = document.querySelector("#email");
+elementoemail.addEventListener('blur', elementoemail=>{
+    validarEmail(elementoemail.target);
+});
+
+function validarEmail(elemento){
+
 }
