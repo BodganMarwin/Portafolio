b893d67b9744936c4f9910a1ad5e7f542e103053
Marwin <marwinbodgan@gmail.com>
2022-10-23 20:57:40 -0400

    Se creo las validaciones para todos los elementos

--- validacion.js
+++ validacion.js
@@ -9,27 +9,62 @@
 //     })
 // });
 
-const elementonombre = document.querySelector('#nombre');
-elementonombre.addEventListener('blur', elementonombre =>{
-    validarNombre(elementonombre.target)
-})
-function validarNombre(elemento){
-    if (elemento.validity.valid){
-        elemento.classList.remove('formcontato__input-error');
-        // elemento.classList.add('formcontato__input');
-    }
-    else {
-        elemento.classList.add('formcontato__input-error');
-        elemento.setCustomValidity('El campo Nombre no puede estar vacio');
-    }
-}
-
+// const elementonombre = document.querySelector('#nombre');
+// elementonombre.addEventListener('blur', elementonombre =>{
+//     validarNombre(elementonombre.target)
+// })
+// function validarNombre(elemento){
+//     if (elemento.validity.valid){
+//         elemento.classList.remove('formcontato__input-error');
+//         // elemento.classList.add('formcontato__input');
+//     }
+//     else {
+//         elemento.classList.add('formcontato__input-error');
+//         elemento.setCustomValidity('El campo Nombre no puede estar vacio');
+//     }
+// }
 //
-const elementoemail = document.querySelector("#email");
-elementoemail.addEventListener('blur', elementoemail=>{
-    validarEmail(elementoemail.target);
+// //
+// const elementoemail = document.querySelector("#email");
+// elementoemail.addEventListener('blur', elementoemail=>{
+//     validarEmail(elementoemail.target);
+// });
+// function validarEmail(elemento){
+//     if (elemento.validity.valid){
+//         elemento.classList.remove('formcontato__input-error');
+//         // elemento.classList.add('formcontato__input');
+//     }
+//     else {
+//         elemento.classList.add('formcontato__input-error');
+//         elemento.setCustomValidity('El campo Correo Electronico no puede estar vacio');
+//     }
+// }
+// const elementoasunto = document.querySelector("#asunto");
+// elementoasunto.addEventListener('blur', elementoasunto=>{
+//     validarAsunto(elementoasunto.target);
+// });
+// function validarAsunto(elemento){
+//     if (elemento.validity.valid){
+//         elemento.classList.remove('formcontato__input-error');
+//         // elemento.classList.add('formcontato__input');
+//     }
+//     else {
+//         elemento.classList.add('formcontato__input-error');
+//         elemento.setCustomValidity('El campo Asunto no puede estar vacio');
+//     }
+// }
+
+import {validarElemento} from "./validadores.js";
+
+const elementosinputs = document.querySelectorAll('input');
+
+elementosinputs.forEach(elemento => {
+   elemento.addEventListener('blur', (elemento)=>{
+       validarElemento(elemento.target);
+   })
 });
 
-function validarEmail(elemento){
-
-}
+const mensaje = document.querySelector('#mensaje');
+mensaje.addEventListener('blur', mensaje=>{
+    validarElemento(mensaje.target);
+})
